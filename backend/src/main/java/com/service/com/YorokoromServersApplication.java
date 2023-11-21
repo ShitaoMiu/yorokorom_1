03d7fa9485864db4e214d04ed631fd6fa674d476
yellowplace <yellowplace@naver.com>
2023-11-22 02:10:30 +0900

    ㅁㄴㅇㅁㄴㅇㅁㄴㅇ

--- backend/src/main/java/com/service/com/YorokoromServersApplication.java
+++ backend/src/main/java/com/service/com/YorokoromServersApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YorokoromServersApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ㅌㅊㅌㅌㅌㅌㅌ");
+		System.out.println("ㅌㅊㅌㅌㅌㅌㅌ");  
 		SpringApplication.run(YorokoromServersApplication.class, args);
 	}
  
